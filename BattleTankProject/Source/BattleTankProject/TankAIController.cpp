01c5a85d33eb74e926df0f8ec82f662d8fd19b6a
Unknown <dudeldude89@googlemail.com>
2018-09-03 20:49:30 +0200

    Added AIController

---
+++ BattleTankProject/Source/BattleTankProject/TankAIController.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	ATank* controlledTank = GetControlledTank();
+
+	if(!controlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a Tank"));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing %s"), *controlledTank->GetName());
+	}
+
+	ATank* firstPlayerTank = GetPlayerTank();
+
+	if(!firstPlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController cannot find first player."));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *firstPlayerTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
+
+
